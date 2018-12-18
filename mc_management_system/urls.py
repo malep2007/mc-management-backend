a218ab9d0519c8bad236ebdec3a0747bd820839d
Ephraim Malinga <ephraim.malinga@gmail.com>
2018-12-18 19:08:31 +0300

    configured Swagger API documentation

--- mc_management_system/urls.py
+++ mc_management_system/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.views import get_swagger_view 
+from rest_framework_swagger.views import get_swagger_view 
 
 schema_view = get_swagger_view(title="MC Management API")
 
